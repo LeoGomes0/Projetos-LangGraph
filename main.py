7d40f155c013eb1cd59b086a2524bf6d537e518d
LeoGomes0 <leonardobrgomes@outlook.com>
2026-01-03 13:15:53 -0300

    Atualizando comentários e nomes de variáveis

--- main.py
+++ main.py
@@ -4,31 +4,30 @@ from langgraph.graph import StateGraph
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from rich import print
-
 import os
 
-# Buscando chave da Gemini API no arquivo .env
+# Buscando a chave da Gemini API no arquivo .env
 load_dotenv()
 API_KEY=os.getenv("GEMINI_API_KEY")
 
 # Inicializando o modelo Gemini 2.5 flash da Google
 llm_model = ChatGoogleGenerativeAI(model="gemini-2.5-flash", api_key=API_KEY)
 
-# Criando a classe de estado para o StateGraph para processar mensagens
+# Criando a classe de estado para o StateGraph para processar as mensagens
 class State(BaseModel):
     input: str
     output: str
 
-# Função para processar a mensagem de entrada e gerar uma resposta usando o modelo Gemini
+# Criando função para processar a mensagem de entrada e gerar uma resposta usando o modelo Gemini
 def responde_mensagem(state):
-    mensagem_entrada = state.input
-    resposta_llm = llm_model.invoke([HumanMessage(content=mensagem_entrada)])
+    input_mensagem = state.input
+    resposta_llm = llm_model.invoke([HumanMessage(content=input_mensagem)])
     return State(input=state.input, output=resposta_llm.content)
 
-# Criando o StateGraph para gerenciar os fluxos de mensagens
+# Criando o StateGraph para conseguir gerenciar os fluxos de mensagens que chegam
 graph = StateGraph(State)
 
-# Adicionando a função de resposta como um nó no grafo
+# Criando o nó no grafo para a função de resposta
 graph.add_node("responde_mensagem", responde_mensagem)
 
 # Definindo os pontos de entrada e saída do grafo
@@ -36,10 +35,9 @@ graph.set_entry_point("responde_mensagem")
 graph.set_finish_point("responde_mensagem")
 
 # Compilando o grafo para usa-lo
-export_graph = graph.compile()
+graph_compilado = graph.compile()
 
-# Um exemplo de uso do grafo para processar uma mensagem
+# Testando o grafo compilado com uma mensagem de entrada fixa
 if __name__ == "__main__":
-    resultado = export_graph.invoke(State(input="Quero que se apresente em poucas palavras", output=""))
+    resultado = graph_compilado.invoke(State(input="Olá, tudo bem com você?", output=""))
     print(resultado)
-
